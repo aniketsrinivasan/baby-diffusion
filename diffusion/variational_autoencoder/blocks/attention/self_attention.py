54c1e8a7885970a613e7cd2188110690bd922224
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-06-30 21:30:00 +0100

    Added self-attention class.

--- diffusion/variational_autoencoder/blocks/attention/self_attention.py
+++ diffusion/variational_autoencoder/blocks/attention/self_attention.py
@@ -1,2 +1,22 @@
-class SelfAttention():
-    pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, projection_bias=True, ejection_bias=True):
+        super().__init__()
+
+        # Linear layers:
+        self.projection = nn.Linear(in_features=d_embed, out_features=2*d_embed, bias=projection_bias)
+        self.ejection = nn.Linear(in_features=d_embed, out_features=d_embed, bias=ejection_bias)
+
+        # Number of heads and dimension of each head:
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads    # d_embed is split into the number of heads
+
+    # Forward pass:
+    def forward(self, x: torch.Tensor, causal_mask=False) -> torch.Tensor:
+        pass
