349822c0c0da2a26ea9948c9734dd4500147e9fd
Aniket <156108186+aniketsrinivasan@users.noreply.github.com>
2024-07-11 12:42:12 +0100

    Updated commit.

--- diffusion/blocks/__init__.py
+++ diffusion/blocks/__init__.py
@@ -1,3 +1,5 @@
-from attention_block import VAE_AttentionBlock
-from residual_block import VAE_ResidualBlock
-from attention import SelfAttention
+from .vae_attention_block import VAE_AttentionBlock
+from .vae_residual_block import VAE_ResidualBlock
+from .unet_attention_block import UNet_AttentionBlock
+from .unet_residual_block import UNet_ResidualBlock
+from .attention import SelfAttention, CrossAttention
---
+++ diffusion/utils/__init__.py
@@ -0,0 +1,1 @@
+from .diffusion_utils import TimeEmbedding, UpSample, UNet_OutputLayer
